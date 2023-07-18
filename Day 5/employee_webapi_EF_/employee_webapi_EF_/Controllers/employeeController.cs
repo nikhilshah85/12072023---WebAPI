086b35200003494c0c438134feae89fcbcae3baa
Nikhil <nikhilshah.net@hotmail.com>
2023-07-18 13:00:21 +0530

    Day 5 - API - EF Core

---
+++ Day 5/employee_webapi_EF_/employee_webapi_EF_/Controllers/employeeController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using employee_webapi_EF_.Models.EF;
+
+namespace employee_webapi_EF_.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class employeeController : ControllerBase
+    {
+        private readonly EmployeeManagementApiDbContext _context = new EmployeeManagementApiDbContext();
+
+        //public employeeController(EmployeeManagementApiDbContext context)
+        //{
+        //    _context = context;
+        //}
+
+        // GET: api/employee
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<EmployeeDetail>>> GetEmployeeDetails()
+        {
+          if (_context.EmployeeDetails == null)
+          {
+              return NotFound();
+          }
+            return await _context.EmployeeDetails.ToListAsync();
+        }
+
+        // GET: api/employee/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<EmployeeDetail>> GetEmployeeDetail(int id)
+        {
+          if (_context.EmployeeDetails == null)
+          {
+              return NotFound();
+          }
+            var employeeDetail = await _context.EmployeeDetails.FindAsync(id);
+
+            if (employeeDetail == null)
+            {
+                return NotFound();
+            }
+
+            return employeeDetail;
+        }
+
+        // PUT: api/employee/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutEmployeeDetail(int id, EmployeeDetail employeeDetail)
+        {
+            if (id != employeeDetail.EmpNo)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(employeeDetail).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!EmployeeDetailExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/employee
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<EmployeeDetail>> PostEmployeeDetail(EmployeeDetail employeeDetail)
+        {
+          if (_context.EmployeeDetails == null)
+          {
+              return Problem("Entity set 'EmployeeManagementApiDbContext.EmployeeDetails'  is null.");
+          }
+            _context.EmployeeDetails.Add(employeeDetail);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (EmployeeDetailExists(employeeDetail.EmpNo))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetEmployeeDetail", new { id = employeeDetail.EmpNo }, employeeDetail);
+        }
+
+        // DELETE: api/employee/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmployeeDetail(int id)
+        {
+            if (_context.EmployeeDetails == null)
+            {
+                return NotFound();
+            }
+            var employeeDetail = await _context.EmployeeDetails.FindAsync(id);
+            if (employeeDetail == null)
+            {
+                return NotFound();
+            }
+
+            _context.EmployeeDetails.Remove(employeeDetail);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool EmployeeDetailExists(int id)
+        {
+            return (_context.EmployeeDetails?.Any(e => e.EmpNo == id)).GetValueOrDefault();
+        }
+    }
+}
